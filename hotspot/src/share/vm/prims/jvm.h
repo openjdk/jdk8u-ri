0d4cfc090c651786535529acfe5acb0209cb1d3d
David Holmes <dholmes@openjdk.org>
2022-06-02 21:19:38 +0000

    8287132: Retire Runtime.runFinalizersOnExit so that it always throws UOE
    Reviewed-by: mchung, iris

--- hotspot/src/share/vm/prims/jvm.h
+++ hotspot/src/share/vm/prims/jvm.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1997, 2014, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997, 2022, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -147,9 +147,6 @@ JVM_OnExit(void (*func)(void));
 /*
  * java.lang.Runtime
  */
-JNIEXPORT void JNICALL
-JVM_Exit(jint code);
-
 JNIEXPORT void JNICALL
 JVM_Halt(jint code);
 
